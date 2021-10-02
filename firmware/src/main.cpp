2b79782ed2d7d56f928c9567ec2576108ef79ecd
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2021-10-02 15:26:35 +0100

    Set the time using the buttons

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -16,6 +16,157 @@
 #include "ds1338.h"
 #include "serial.h"
 
+static uint16_t correction;
+static bool time_set = false;
+static bool force_update = false;
+static uint8_t button_state = 0;
+static bool blink_hours = false;
+static bool blink_minutes = false;
+
+uint8_t get_button_event()
+{
+	static int8_t buttons = 0;
+	static int8_t buttons_old = 0;
+	static bool hold = false;
+	static uint8_t repeat_timer = 0;
+
+	int8_t button_event = 0;
+
+	buttons = ((PINC & (1 << PINC2)) ? 0 : 0x02) |
+			  ((PINC & (1 << PINC3)) ? 0 : 0x04) |
+			  ((PIND & (1 << PIND4)) ? 0 : 0x01);
+
+	/* No more than one button at a time */
+	if (buttons & (buttons - 1))
+	{
+		return 0;
+	}
+
+	button_event = 0;
+	if (buttons != buttons_old && buttons)
+	{
+		/* A button has been pressed */
+		serial::puts("Pressed");
+		repeat_timer = 0;
+		button_event = buttons;
+	}
+	else if (buttons == buttons_old && buttons)
+	{
+		/* A button is being held */
+		if (((Draw::current_angle / 256) & 0x07) == 0)
+		{
+			if (!hold)
+			{
+				if (repeat_timer < 8)
+				{
+					repeat_timer++;
+				}
+				else
+				{
+					button_event = buttons;
+				}
+				hold = true;
+			}
+		}
+		else
+		{
+			hold = false;
+		}
+	}
+
+	buttons_old = buttons;
+
+	return button_event;
+}
+
+void handle_buttons()
+{
+	static int8_t wait_save_correction = 0;
+
+	int8_t button_event = get_button_event();
+
+	if (!button_event)
+	{
+		return;
+	}
+
+	/* SW1 changes the state */
+	if (button_event & 0x01)
+	{
+		if (++button_state > 2)
+		{
+			button_state = 0;
+			force_update = true;
+		}
+	}
+
+	switch(button_state)
+	{
+	case 0:
+		time_set = false;
+		blink_hours = blink_minutes = false;
+		if (button_event & 0x04)
+		{
+			correction+=3;
+			Draw::offset_angle+=3;
+			wait_save_correction = 3;
+		}
+
+		if (button_event & 0x02)
+		{
+			correction-=3;
+			Draw::offset_angle-=3;
+			wait_save_correction = 3;
+		}
+		break;
+
+	case 1:
+	case 2:
+		time_set = true;
+		blink_hours = button_state == 1;
+		blink_minutes = button_state == 2;
+
+		/* SW2 or 3 */
+		if (button_event & 0x06)
+		{
+			struct time time;
+			DS1338::get(time);
+			uint8_t hours = time.get_hour();
+			uint8_t mins = time.get_min();
+
+			if ((button_state == 1) && (button_event & 0x02)) hours--;
+			if ((button_state == 1) && (button_event & 0x04)) hours++;
+			if ((button_state == 2) && (button_event & 0x02)) mins--;
+			if ((button_state == 2) && (button_event & 0x04)) mins++;
+
+			if (mins > 127) mins = 59;
+			if (mins > 59) mins = 0;
+			if (hours > 127) hours = 23;
+			if (hours > 23) hours = 0;
+
+			time.set_hour(hours);
+			time.set_min(mins);
+			DS1338::set(time);
+		}
+		break;
+	}
+
+	if (!DS1338::tick)
+	{
+		return;
+	}
+
+	if (wait_save_correction)
+	{
+		wait_save_correction--;
+		if (wait_save_correction == 0)
+		{
+			serial::puts("Saving correction to EEPROM\n");
+			eeprom_write_word((uint16_t*) 0, correction);
+		}
+	}
+}
+
 int main()
 {
 	serial::init();
@@ -108,14 +259,12 @@ int main()
 	PORTC |= ((1 << PINC2) | (1 << PINC3));
 	PORTD |= (1 << PIND4);
 
-	int8_t button_state = 0;
-	uint16_t correction = eeprom_read_word((uint16_t*) 0);
-	int8_t wait_save_correction = 0;
-
 	//ThreePhase::set_amplitude(255);
 
 	uint8_t wait_stable = 0;
 	bool ramp_down = false;
+
+	correction = eeprom_read_word((uint16_t*) 0);
 
 	//bool colon_on = false;
 	serial::puts("Entering main-loop\n");
@@ -124,47 +273,8 @@ int main()
 	{
 		if (done)
 		{
-			if (!(PIND & (1 << PIND4)))
-			{
-				correction++;
-				Draw::offset_angle++;
-				serial::puts("SW1\n");
-				wait_save_correction = 3;
-
-				serial::puts("Inc correction: ");
-				serial::puti(correction);
-				serial::putc('\n');
-			}
-
-			if (!(PINC & (1 << PINC2)))
-			{
-				correction--;
-				Draw::offset_angle--;
-				serial::puts("SW2\n");
-				wait_save_correction = 3;
-
-				serial::puts("Dec correction: ");
-				serial::puti(correction);
-				serial::putc('\n');
-			}
-
-			if ((!(PIND & (1 << PIND4)) && (button_state & (1 << 4))))
-			{
-				serial::puts("SW1\n");
-			}
-
-			if ((!(PINC & (1 << PINC2)) && (button_state & (1 << 2))))
-			{
-				serial::puts("SW2\n");
-			}
-
-			if ((!(PINC & (1 << PINC3)) && (button_state & (1 << 3))))
-			{
-				serial::puts("SW3\n");
-			}
+			handle_buttons();
 		}
-
-		button_state = (PINC & ((1 << PINC2) | (1 << PINC3))) | (PIND & (1 << PIND4));
 
 		/* Don't ramp down the amplitude before we have picked up a little speed.
 		 * Apparently, the first bit is haaard; push at full force. PUSH MOTHERFUCKER! PUSH! */
@@ -198,43 +308,41 @@ int main()
 			}
 		}
 
-		if (!DS1338::tick)
+		if (!DS1338::tick && !time_set && !force_update)
 		{
 			continue;
 		}
+
 		DS1338::tick = false;
+		force_update = false;
 
-		if (wait_save_correction)
-		{
-			wait_save_correction--;
-			if (wait_save_correction == 0)
-			{
-				serial::puts("Saving correction to EEPROM\n");
-				eeprom_write_word((uint16_t*) 0, correction);
-			}
-		}
+		if (wait_stable) wait_stable--;
 
-		if (wait_stable)
-		{
-			wait_stable--;
-		}
-
-		serial::puts("tick\n");
+		//serial::puts("tick\n");
 
 		struct time time;
 		DS1338::get(time);
 
-#if 1
-		Draw::draw_digit(0, time.hour >> 4);
-		Draw::draw_digit(35, time.hour & 0xf);
-		Draw::draw_digit(91, time.min >> 4);
-		Draw::draw_digit(126, time.min & 0xf);
-#else
-		Draw::draw_digit(0, time.min >> 4);
-		Draw::draw_digit(35, time.min & 0xf);
-		Draw::draw_digit(91, time.sec >> 4);
-		Draw::draw_digit(126, time.sec & 0xf);
-#endif
+		if (!blink_hours || (Draw::current_angle / 8192) & 1)
+		{
+			Draw::draw_digit(0, time.hour >> 4);
+			Draw::draw_digit(35, time.hour & 0xf);
+		}
+		else
+		{
+			Draw::draw_digit(0, 11);
+			Draw::draw_digit(35, 11);
+		}
+		if (!blink_minutes || (Draw::current_angle / 8192) & 1)
+		{
+			Draw::draw_digit(91, time.min >> 4);
+			Draw::draw_digit(126, time.min & 0xf);
+		}
+		else
+		{
+			Draw::draw_digit(91, 11);
+			Draw::draw_digit(126, 11);
+		}
 
 		/* Blinking colon */
 		if ((time.sec & 1) == 0)
